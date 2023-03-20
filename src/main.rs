5bd82cdd1d28b77dd24734238f4147dd335911d4
Jonathon Anderson <coding@jpanderson.dev>
2023-03-19 23:51:29 -0700

    some asdf command running, not complete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
+use std::process::Command;
 use std::vec::Vec;
 
 use serde::Deserialize;
+use which::which;
+
+const ASDF: &str = "asdf";
 
 #[derive(Debug, Deserialize)]
 struct Config {
@@ -25,14 +29,18 @@ struct Plugin {
 fn main() {
     println!("Here we go!");
 
+    validate_asdf_exists();
+
     let contents = fs::read_to_string(Path::new("./.asdfler.yml"))
         .expect("Should have been able to read the file");
 
     let config: Config = serde_yaml::from_str(&contents).unwrap();
 
     for plugin in config.plugins {
-        let plugin_name = plugin.name;
+        let plugin_name = &plugin.name;
         println!("\n\nNew plugin: {plugin_name}");
+
+        run_plugin_add(&plugin_name);
 
         match plugin.default_version {
             None => println!("No default version set"),
@@ -47,4 +55,38 @@ fn main() {
             }
         }
     }
+}
+
+fn validate_asdf_exists() {
+    match which(ASDF) {
+        Ok(_) => println!("asdf available"),
+        Err(_) => {
+            println!("!!! asdf could not be found in path");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run_plugin_add(plugin_name: &str) {
+    let output = Command::new(ASDF)
+        .args(["plugin", "add", &plugin_name])
+        .output()
+        .expect("!!! Something went very wrong");
+
+    // Print the output so the user sees it. Likely we just want this
+    // for version installs since thsoe can be wordy.
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    println!("{stdout}");
+
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    println!("{stderr}");
+
+    let status = output.status;
+    let code = status
+        .code()
+        .expect("!!! Unexpectedly terminated by signal");
+
+    if code == 2 {
+        println!("`{plugin_name}` is already installed");
+    } // Do a code == 1 here to tell the user it failed
 }
